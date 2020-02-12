555b1deae7a523af40be506a09f295ad305f76e0
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-02-11 20:44:03 -0500

    finished MVP

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,6 +1,3 @@
-# '''
-# Linked List hash table key/value pair
-# '''
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
@@ -8,10 +5,6 @@ class LinkedPair:
         self.next = None
 
 class HashTable:
-    '''
-    A hash table that with `capacity` buckets
-    that accepts string keys
-    '''
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
@@ -19,132 +12,103 @@ class HashTable:
 
 
     def _hash(self, key):
-        '''
-        Hash an arbitrary key and return an integer.
-        You may replace the Python hash with DJB2 as a stretch goal.
-        '''
         return hash(key)
 
-
-    def _hash_djb2(self, key):
-        '''
-        Hash an arbitrary key using DJB2 hash
-        OPTIONAL STRETCH: Research and implement DJB2
-        '''
-        pass
-
-
     def _hash_mod(self, key):
-        '''
-        Take an arbitrary key and return a valid integer index
-        within the storage capacity of the hash table.
-        '''
         return self._hash(key) % self.capacity
 
 
     def insert(self, key, value):
-        '''
-        Store the value with the given key.
-        Hash collisions should be handled with Linked List Chaining.
-        Fill this in.
-        '''
-        if self.size is self.capacity:
-            self.resize()
-        
-        self.size += 1
         index = self._hash_mod(key)
+        # and we look in our HT at the position the index indicates
         node = self.storage[index]
-        if node is None:
-            self.storage[index] = LinkedPair(key, value)
-            return
-        # prev = node
-        while node is not None:
-            prev = node
+        # if it is, we need to make a while loop to keep checking if the spot in our LL that is on that index is empty
+        while node is not None and node.key != key:
+            # if it is not, we make a copy of the current node pointer
+            temp = node
+            # and the current node pointer will point to the current's next node
             node = node.next
-        prev.next = LinkedPair(key, value)
+        # we check if it is empty..
+        if node is not None:
+            node.value = value
+        else:
+            new_node = LinkedPair(key, value)
+            # if it is, we create the node on that index and return
+            new_node.next = self.storage[index]
+            self.storage[index] = new_node
+            # we increase the size by 1 since we are just adding one
+            self.size += 1
+        
 
     def remove(self, key):
-        '''
-        Remove the value stored with the given key.
-
-        Print a warning if the key is not found.
-
-        Fill this in.
-        '''
+        # we run the hash method to t=determine the index of the node we are trying to remove
         index = self._hash_mod(key)
+        # and we reference the hashed key in our HT
         node = self.storage[index]
-        while node is not None and node.key != key:
-            prev = node
-            node = node.next
+        # here we check if our spot is not empty, and also if the key doesn't match the key in our node in that spot
+        temp = None
+        while node.key != key:
+            # if it doesn't, means we need to iterate over our LL until we find it, so we assign a temp var copy of our node
+            temp = node
+            # and we assign the current node to be the next node
+            node = temp.next
+        # if node is not, it means we did not found the node we want to delete, and we return None
         if node is None:
             return None
+        # if we find our node...
         else:
-            self.size -= 1
-            result = node.value
-            if prev is None:
-                node = None
+            # we check if temp is None, meaning the node being deleted is the LL's head..
+            if temp is None:
+                # we grab the value of the node
+                result = node.value
+                # we assign the new head to be the next node
+                self.storage[index] = node.next
+                # and return the deleted node value
+                return result
             else:
-                prev.next = prev.next.next
-            return result
+                self.size -= 1
+                # we grab the node's value
+                temp.next = node.next
+                # return node value
+                return node.value
 
 
     def retrieve(self, key):
-        '''
-        Retrieve the value stored with the given key.
-
-        Returns None if the key is not found.
-
-        Fill this in.
-        '''
+        # we generate the index with our hashed key
         index = self._hash_mod(key)
+        # we look in our HT the key we are looking for with our index
         node = self.storage[index]
-        while node is not None and node.key != key:
+        # and we check if there is no Node, and if the key
+        while node is not None:
+            if node.key == key:
+                return node.value
+            # if it does not match, we iterate
             node = node.next
-        if node is None:
-            return None
-        else:
-            return node.value
-
+        # if node ends up being none, there is no node so we return none
+        return None
 
     def resize(self):
-        '''
-        Doubles the capacity of the hash table and
-        rehash all key/value pairs.
-
-        Fill this in.
-        '''
+        # increase capacity times 2
         self.capacity *= 2
+        # we make a new storage with new capacity
         new_storage = [None] * self.capacity
-
-        for i in range(self.size):
+        # we iterate over the old storage
+        for i in range(len(self.storage)):
+            # we assign each value in current index to the new storage
             new_storage[i] = self.storage[i]
+        # we set our new storage to be our storage
         self.storage = new_storage
 
-
-
-if __name__ == "__main__":
-    ht = HashTable(2)
-
-    ht.insert("line_1", "Tiny hash table")
-    ht.insert("line_2", "Filled beyond capacity")
-    ht.insert("line_3", "Linked list saves the day!")
-
-    # print("HashTable", ht.storage)
-    print("")
-
-    # Test storing beyond capacity
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
-
-    # Test resizing
-    old_capacity = len(ht.storage)
-    ht.resize()
-    new_capacity = len(ht.storage)
-
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
-
-    # Test if data intact after resizing
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+# if __name__ == "__main__":
+#     ht = HashTable(8)
+#     ht.insert("key-0", "val-0")
+#     ht.insert("key-1", "val-1")
+#     ht.insert("key-2", "val-2")
+#     ht.insert("key-3", "val-3")
+#     ht.insert("key-4", "val-4")
+#     ht.insert("key-5", "val-5")
+#     ht.insert("key-6", "val-6")
+#     ht.insert("key-7", "val-7")
+#     ht.insert("key-8", "val-8")
+#     ht.insert("key-9", "val-9")
+#     print(ht.retrieve("key-0"))
